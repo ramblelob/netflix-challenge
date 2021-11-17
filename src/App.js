965b9e94471935e2cadf44da1e9a542def1ca363
ramblelob <aileen.ramelb@gmail.com>
2021-11-16 20:12:20 -0800

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
 
 function App() {
+
   return (
     <div className="app">
-      <HomeScreen />
+        <Routes>
+          <Route path="/" 
+          element={<HomeScreen />} />
+        </Routes>
     </div>
   );
 }
